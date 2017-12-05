6b24305a83520412ecfded25b4cd6ee9b6221b59
Andrzej Dol <dolecki.andrzej.1990@gmail.com>
2017-12-06 00:25:49 +0100

    9.1 end

--- src/main/java/com/task/chapter9/task2/Main.java
+++ src/main/java/com/task/chapter9/task2/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String fileInput = System.getProperty("user.dir") + "\\src\\main\\java\\com\\task\\chapter9\\task2" +
+                "\\txt\\file.txt";
+
     }
 
 }
